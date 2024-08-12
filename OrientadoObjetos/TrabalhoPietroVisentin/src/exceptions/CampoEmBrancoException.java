f818dec781a58d12fc5ad6d08ab84509a8afbf67
Pietro Visentin <86116655+Pietrocv@users.noreply.github.com>
2024-08-12 11:04:48 -0300

    Delete OrientadoObjetos directory

--- OrientadoObjetos/TrabalhoPietroVisentin/src/exceptions/CampoEmBrancoException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class CampoEmBrancoException extends Exception {
-    public CampoEmBrancoException(String campo) {
-        super("O campo \"" + campo + "\" não pode estar em branco.");
-    }
-}
--- OrientadoObjetos/TrabalhoPietroVisentin/src/exceptions/ProfessorNaoAtribuidoException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class ProfessorNaoAtribuidoException extends Exception {
-    public ProfessorNaoAtribuidoException(String message) {
-        super(message);
-    }
-}
